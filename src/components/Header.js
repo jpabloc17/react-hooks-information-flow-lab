89ce3a7dbd7d75a28205000cf6fb1978681256e1
Pablo Castro <pablocas-tro1@outlook.com>
2023-08-13 22:35:05 -0400

    Complete Lab

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header({ mode, onDarkModeClick }) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>{mode ? "Dark" : "Light"} Mode</button>
+    </header>
+  );
+}
+
+export default Header;
